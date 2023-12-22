d490517fe899e3515e7799f38d2064bc70f69cd5
João Azevedo <joaoazevedo@outlook.com>
2023-12-22 23:38:18 +0000

    Add theme provider and side panel components to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,22 @@
+import { ThemeProvider } from "@/components/theme-provider";
+import SidePanel from "@/components/SidePanel";
+
 function App() {
   return (
-    <h1 className="text-3xl text-blue-600 font-bold underline">Hello world!</h1>
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <div className="flex p-8 gap-4">
+        <div id="video" className="w-8/12">
+          <video
+            src="https://player.vimeo.com/external/451966658.hd.mp4?s=3f4a6c8f2e2b5a2f1a3e3a8f7d2d9b0e3d5c1f9f&profile_id=175"
+            controls
+            className="w-full"
+          />
+        </div>
+        <div id="right-panel" className="w-4/12">
+          <SidePanel />
+        </div>
+      </div>
+    </ThemeProvider>
   );
 }
 
